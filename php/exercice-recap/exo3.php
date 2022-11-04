20419426191c936c6581ead474ff8a6486083bf2
florentin harmand <florentin.harmand@hotmail.com>
2022-11-04 22:00:19 +0100

    exo boucle while if et php base terminé, reste shifumi à faire

--- php/exercice-recap/exo3.php
+++ php/exercice-recap/exo3.php
@@ -4,11 +4,17 @@ echo ("Pensez à un personnage : Melle Rose, le professeur Violet, le Colonnel M
 echo ("\n true : correspond à oui ; false : correspond à non");
 echo ("\n" );
 
+$compteur = 0;
+while ($compteur ==0){
 $sexe = readline("le personnage est il un homme?  ");
+
 if (($sexe =="true")){
+    $compteur++;
+    $compteur--;
     $chapeau = readline("Le personnage porte-t-il un chapeau?  ");
     if ($chapeau == "true"){
         echo ("Vous pensez au Professeur Violet ");
+        $compteur++;
         
 
     }  
@@ -16,22 +22,32 @@ if (($sexe =="true")){
     $moustache = readline("le personnage porte-t-il une moustache?  ");
     if ($moustache == "true"){
         echo("Vous pensez au Colonnel Moutarde");
+        $compteur++;
         }
     }
      if ($chapeau == "false" && $moustache == "false" ){
         echo("Vous pensez au Révérand Olive");
+        $compteur++;
+        
         }
 
 }
   
-else {
+else if ($sexe == "false") {
+    
     $lunettes = readline("le personnage porte-t-il des lunettes? ");
+    
     if ($lunettes == "true"){
         echo ("Vous pensez à Madame Leblanc");
+        $compteur++;
     }
-    else {
+    else if ($lunettes == "false") {
         echo ("Vous pensez à Melle Rose");
+        $compteur--;
+        $compteur++;
+        
     }
+}
 }
     
 
--- php/exercice-recap/exo4.php
+++ php/exercice-recap/exo4.php
@@ -1,20 +1,32 @@
 <?php
+$compteur = 0;
 
+while ($compteur ==0 ){
 $nombre = readline("donnez un nombre pair et divisible par trois ");
 
+    
+
+
+
+
 if ($nombre %2 == 0 && $nombre %3 == 0 ){
-    echo ("votre nombre est pair et divisible par trois" );
+    echo ("votre nombre est pair et divisible par trois " )  ;
+    $compteur++;
+    
 }
 else if ($nombre %2 != 0 && $nombre %3 != 0){
-    echo ("votre nombre est ni pair ni divisble par trois ");
+    echo ("votre nombre est ni pair ni divisble par trois \n");
 }
     else if ($nombre %2 != 0 ){
-        echo ("votre nombre n'est pas pair ");
+        
+        echo ("votre nombre n'est pas pair \n ");
+        
+        
     }
         else if ($nombre %3 != 0) {
-            echo ("votre nombre n'est pas divisible par trois ");
+            echo ("votre nombre n'est pas divisible par trois \n ");
         }
     
-    
+    }    
 
 ?>
